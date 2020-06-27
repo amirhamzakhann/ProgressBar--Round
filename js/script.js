85fab3c0ab06a1706af7bd7ffd4f495ba7f1acd0
Md. Amir Hamza <shuvokhan2044@gmail.com>
2020-06-27 17:07:56 +0600

    round-progressbar

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function(){
+    $(window).on('load',function(){
+        $('.bar-1').animate({width: '100%'}, 4000);
+    });
+    $(window).on('load',function(){
+        $('.bar-2').animate({width: '100%'}, 4000);
+    });
+    $(window).on('load',function(){
+        $('.bar-3').animate({width: '80%'}, 4000);
+    });
+    $(window).on('load',function(){
+        $('.bar-4').animate({width: '90%'}, 4000);
+    });
+    $(window).on('load',function(){
+        $('.bar-5').animate({width: '95%'}, 4000);
+    });
+    $(window).on('load',function(){
+        $('.bar-6').animate({width: '70%'}, 4000);
+    });
+    $(window).on('load',function(){
+        $('.bar-7').animate({width: '100%'}, 4000);
+    });
+});
